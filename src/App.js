8a53677664959fc0207ff4aaa1deb9b49ba5edbe
Golasto <vdgraafmatteus@gmail.com>
2022-12-07 20:34:40 +0100

    Huiswerk gemaakt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Pokemon from './components/Pokemon';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [offset, setOffset] = useState(0);
+
+    async function getData() {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=" + offset)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setData(result.results);
+                    setIsLoaded(true);
+                }
+            )
+    }
+
+    function changePokemon(next) {
+        if (next) {
+            setOffset(offset + 20)
+        } else {
+            setOffset(offset - 20)
+        }
+        console.log(offset)
+        getData();
+    }
+
+    function disableButtons() {
+        if (offset === 0) {
+            document.getElementById("p").disabled = true;
+        } else {
+            document.getElementById("p").disabled = false;
+        }
+        if (offset > 1140) {
+            document.getElementById("n").disabled = true;
+        } else {
+            document.getElementById("n").disabled = false;
+        }
+    }
+
+    useEffect(() => {
+        getData();
+        if (document.getElementById("p") !== null) {
+            disableButtons()
+        }
+    }, [offset])
+
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    } else if (isLoaded) {
+        if (data.length !== 0) {
+            return (
+                <>
+                    <div>
+                        <button id="p" onClick={() => changePokemon(false)}>Previous</button>
+                        <button id="n" onClick={() => changePokemon(true)}>Next</button>
+                        {data.map(d =>
+                            <Pokemon key={d.name} name={d.name} />
+                        )}
+                    </div>
+                </>
+            )
+        } else {
+            return <div>Loading...</div>
+        }
+    }
 }
 
 export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+
+export default function Pokemon({name}) {
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState([]);
+
+    const abilities = [];
+    let moves = 0;
+
+ function getData() {
+     console.log(name)
+        fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setData(result);
+                    setIsLoaded(true)
+                }
+            )
+    }
+    function getAbilities() {
+        for (let i = 0; i < data.abilities.length; i++) {
+            abilities.push(<p>{data.abilities[i].ability.name}</p>)
+        }
+        moves = data.moves.length;
+    }
+    useEffect(() => {
+        if (data.length === 0)
+        getData();
+    }, [data]);
+
+    if (isLoaded) {
+        if (data.length !== 0) {
+            getAbilities()
+            return(
+                <>
+                    <h1>{data.forms[0].name}</h1>
+                    <img src={data.sprites.other['official-artwork'].front_default} alt="" />
+                    <h2>Abilities: {abilities}</h2>
+                    <p>Weight: {data.weight}</p>
+                    <p>Moves: {moves}</p>
+                </>
+            )
+        }else {
+            return <div>Loading...</div>
+        }
+
+    }
+
+}
